4d07eb1f26c3053b9b56cf3a710d0255c81943ef
andpramor <andpramordev@gmail.com>
2023-10-11 10:20:16 +0200

    Saco la lista de conductores y sus ID

--- proyectoF1.py
+++ proyectoF1.py
@@ -4,14 +4,20 @@
 import json, requests
 
 #ACABO TODAS LAS URL CON .JSON PORQUE LA API DEVUELVE LOS DATOS COMO XML POR DEFECTO.
-#Le pediré al usuario algunos datos para completar la URL con la que atacaré a la API, por ahora es un valor que doy yo para pruebas:
-url = "http://ergast.com/api/f1.json"
+#Le voy a pedir al usuario que elija un conductor, de la temporada del año pasado, de forma que no haya demasiados datos pero los datos sean de una temporada completa, para lo que le muestro todos los disponibles y cuál es su id:
+url = "http://ergast.com/api/f1/2022/drivers.json"
 
-#Hago la solicitud GET:
+#Ante todo, compruebo que puedo alcanzar la API haciendo la solicitud GET:
 peticion = requests.get(url)
 #Si el código de respuesta es 200, todo va bien, así que lo compruebo. Si va bien cojo los datos, sino, aviso del error:
 if peticion.status_code == 200:
     datos = peticion.json()
-    print(datos) #Saco por pantalla los datos del json para probar
+    #Aquí tengo en datos todo el json. Ahora voy a meter todos los conductores y su ID en otra variable:
+    conductores = datos["MRData"]["DriverTable"]["Drivers"]
+    #Ahora recorro todos los datos en conductores y saco solo el nombre y el id:
+    for conductor in conductores:
+        id = conductor["driverId"]
+        nombre = f"{conductor['givenName']} {conductor['familyName']}"
+        print(f"Nombre: {nombre}, ID: {id}.\n")
 else:
-    print("La solicitud GET no ha funcionado.")
+    print("La solicitud GET no ha funcionado. API inalcanzable.")
