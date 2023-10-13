d745926fb2c8febb59f3afb1cba94918b5ffaefd
andpramor <andpramordev@gmail.com>
2023-10-13 20:49:43 +0200

    Presentacion del juego y de los jugadores

---
+++ jugadores.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+print('¡Hola! Vamos a ver quién de los dos sabe más sobre las victorias de la temporada 2022 en Fórmula 1. Os daré los nombres de tres pilotos a cada uno, y responderéis cuántas victorias creéis que obtuvieron a lo largo de la temporada. Luego veremos quién acierta más veces ¿¿O SE QUEDA MÁS CERCA??')
+jugador1 = input('Nombre del jugador 1:')
+print(f'Hola, {jugador1}.')
+jugador2 = input('Nombre del jugador 2:')
+print(f'Hola, {jugador2}.')
