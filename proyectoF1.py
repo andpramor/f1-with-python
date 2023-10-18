2917420d93c171e3ee9b531290c0c86123590a50
andpramor <andpramordev@gmail.com>
2023-10-18 20:55:15 +0200

    Lista de posiciones de un piloto en un año

--- proyectoF1.py
+++ proyectoF1.py
@@ -8,8 +8,8 @@ def tryApi(estado): #La respuesta 200 es el OK de la API, otro número sería un
     print('Algo ha ido mal, la solicitud de datos a la API no ha funcionado, podría estar offline.')
     return False
 
-def getDrivers():
-    season = int(input('¿De qué año te interesa conocer la tabla de victorias? '))
+def getDrivers(season):
+    
     print('')
     if season < 1950:
         print('Demasiado atrás en el tiempo, los mundiales de Fórmula 1 comenzaron en el año 1950. Volvamos a intentarlo.')
@@ -28,11 +28,26 @@ def getDrivers():
                 conductores.append({'id':conductor['driverId'],'nombre':conductor['givenName'],'apellidos':conductor['familyName']})
             return conductores
 
+def getWins(conductor,año):
+    url = f"http://ergast.com/api/f1/{año}/drivers/{conductor}/results.json" #Este link de la api me devuelve la posición del piloto en todas las carreras de ese año.
+    peticion = requests.get(url)
+    if tryApi(peticion.status_code):
+        datos = peticion.json()
+        posiciones = [] #En esta lista guardaré todas las posiciones en las que ha quedado, luego contaré el número de veces que sale el 1.
+        for carrera in datos['MRData']['RaceTable']['Races']:
+            posicion = carrera['Results'][0]['position']
+            posiciones.append(posicion)
+        print(f'Posiciones de {conductor}:')
+        print(posiciones)
+
 print('Bienvenido al contador de victorias de Fórmula 1.')
-conductoresDelAño = getDrivers()
+año = int(input('¿De qué año te interesa conocer la tabla de victorias? '))
+conductoresDelAño = getDrivers(año)
+# getWins('alonso',2008) Probé con alonso 2020 y fue el mayor problema.
 
 
+#for conductor in conductoresDelAño: #Para cada conductor, cuento sus victorias y las meto en un diccionario, luego lo ordenaré.
+#    getWins(conductor['id'],año)
 
-print('Los conductores de ese año fueron:')
-for conductor in conductoresDelAño:
-    print(conductor['id'])
+#Voy a sacar la tabla de victorias como Nombre, Apellidos, Nº de victorias
+#Ordenado de más victorias a menos, y cuando llegue a los pilotos sin ninguna victoria, ordenados alfabéticamente.
